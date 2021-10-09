4dbf7f74e48f9b40652b1cf6348b2f1b2dbc80d7
shreyas027 <88003332+shreyas027@users.noreply.github.com>
2021-10-09 09:51:32 -0400

    Update PlayerArrow.js

--- PlayerArrow.js
+++ PlayerArrow.js
@@ -7,7 +7,7 @@ class PlayerArrow {
     this.width = width;
     this.height = height;
     this.body = Bodies.rectangle(x, y, this.width, this.height, options);
-    this.image = loadImage("./assets/arrow.png");
+    this.image = loadImage("arrow.png");
     this.archerAngle = archerAngle;
     this.velocity = 0;
     World.add(world, this.body);
